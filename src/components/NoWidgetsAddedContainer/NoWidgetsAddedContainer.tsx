564cca18250e27e0d0398aeeaaf8188a0eff61e0
Dmitry Zharikov <dimbltoss@gmail.com>
2025-04-09 17:29:10 +0300

    image

--- src/components/NoWidgetsAddedContainer/NoWidgetsAddedContainer.tsx
+++ src/components/NoWidgetsAddedContainer/NoWidgetsAddedContainer.tsx
@@ -5,16 +5,15 @@ export const NoWidgetsAddedContainer = (props: {
 }) => {
   const { toggleEditDashboard } = props;
   return (
-    <div className="flex flex-col items-center justify-center gap-4 h-[50vh]">
-      <img className="mt-auto" src="/public/img.svg" alt="img" />
+    <div className="no-widgets-added-container flex flex-col items-center justify-center gap-4 h-[50vh]">
       <h2 className="text-2xl font-bold mt-4">No widgets added yet</h2>
-      <p className="text-sm">Click “Edit Dashboard” to add a new widget</p>
+      <p className="text-sm">Click "Edit Dashboard" to add a new widget</p>
       <Button
         name="Edit Dashboard"
         onClick={toggleEditDashboard}
-        className="rounded-lg border border-solid px-2 py-1 
-        text-base font-medium font-sans  bg-blue-500 text-white border-blue-500 
-        cursor-pointer hover:bg-blue-600 hover:border-blue-600"
+        className="rounded-lg border border-solid px-2 py-1 text-base font-medium 
+        font-sans bg-blue-500 text-white border-blue-500 cursor-pointer 
+        hover:bg-blue-600 hover:border-blue-600"
       />
     </div>
   );
